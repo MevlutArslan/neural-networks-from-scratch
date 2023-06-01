432219d5caa811bc2061dd9344fb6f111eaf3952
MevlutArslan <mevlut12098@gmail.com>
2023-05-31 23:37:38 -0500

    stuff

--- main.c
+++ main.c
@@ -1,15 +1,55 @@
 #include <stdio.h>
-#include "math/nmath.h"
+#include "nmath/nmath.h"
+#include "helper/matrix_linked_list.h"
+#include "tests/helper_tests/linkedlist_test.h"
+#include "tests/matrix_tests/matrix_test.h"
+#include <string.h>
 
-int main(void)
+void runTests();
+void runProgram();
+
+int main(int argc, char* argv[])
 {
-    // C has char, int, floating point types (float, double)
-    // no boolean in C
-    // use 1 for true and 0 for false
-    Matrix* matrix = createMatrix(2,2);
-    
-    printf("Hello World! \n");
-    printMatrix(matrix);
+    int isTesting = 0;
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "test") == 0) {
+            isTesting = 1;
+            break;
+        }
+    }
 
-    freeMatrix(matrix);
+    if (isTesting) {
+        printf("Running tests...\n");
+        runTests();
+    } else {
+        printf("Running the program...\n");
+        runProgram();
+    }
+    
+}
+
+void runTests() {
+    testAddToTheEnd();
+    testMatrixCreation();
+}
+
+void runProgram() {
+    MatrixLinkedListNode* toClean = malloc(sizeof (MatrixLinkedListNode));
+    Matrix* matrix = createMatrix(2,2);
+    Matrix* m2 = createMatrix(3,3);
+
+    toClean->next = NULL;
+    toClean->data = matrix;
+
+    printf("Hello World! \n");
+
+    MatrixLinkedListNode* current = toClean;
+    while (current != NULL) {
+        printMatrix(current->data);
+        freeMatrix(current->data);
+
+        MatrixLinkedListNode* nextNode = current->next;
+        free(current);
+        current = nextNode;
+    }
 }
