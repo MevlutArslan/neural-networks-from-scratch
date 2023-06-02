32ce2102a87bc120f885d765dc687d5ad723a3ca
MevlutArslan <mevlut12098@gmail.com>
2023-06-01 21:52:43 -0500

    implement determinant and tests for it.

--- nmath/nmath.c
+++ nmath/nmath.c
@@ -1,9 +1,10 @@
 #include "nmath.h"
 
-Matrix* multiply(Matrix* m1, Matrix* m2) {
+
+Matrix* multiply(const Matrix* m1, const Matrix* m2) {
     // m1.rows has to be equal m2.cols
     if( m1->columns != m2->rows) {
-        printf("CANNOT MULTIPLE M1 and M2. M1's column count \n does not match M2's row count! \n");
+        printf("Cannot Multiply M1 and M2. M1's column count \n does not match M2's row count! \n");
         return NULL;
     }
     Matrix* m3 = createMatrix(m1->rows, m2->columns);
@@ -22,7 +23,7 @@ Matrix* multiply(Matrix* m1, Matrix* m2) {
     return m3;
 }
 
-Matrix* add(Matrix* m1, Matrix* m2) {
+Matrix* add(const Matrix* m1, const Matrix* m2) {
     if(m1->rows != m2->rows || m1->columns != m2->columns) {
         printf("The sizes of the matrices do not match!");
         return NULL;
@@ -38,7 +39,7 @@ Matrix* add(Matrix* m1, Matrix* m2) {
     return m3;
 }
 
-Matrix* subtract(Matrix* m1, Matrix* m2) {
+Matrix* subtract(const Matrix* m1, const Matrix* m2) {
     if(m1->rows != m2->rows || m1->columns != m2->columns) {
         printf("The sizes of the matrices do not match!");
         return NULL;
@@ -54,7 +55,7 @@ Matrix* subtract(Matrix* m1, Matrix* m2) {
     return m3;
 }
 
-Matrix* elementWiseMultiply(Matrix* m1, Matrix* m2){
+Matrix* elementWiseMultiply(const Matrix* m1, const Matrix* m2){
     if(m1->rows != m2->rows || m1->columns != m2->columns) {
         printf("The sizes of the matrices do not match!");
         return NULL;
@@ -70,7 +71,7 @@ Matrix* elementWiseMultiply(Matrix* m1, Matrix* m2){
     return m3;
 }
 
-Matrix* transpose(Matrix* m) {
+Matrix* transpose(const Matrix* m) {
     // switch dimensions
     Matrix* m3 = createMatrix(m->columns, m->rows);
 
@@ -81,4 +82,53 @@ Matrix* transpose(Matrix* m) {
     }
 
     return m3;
+}
+
+Matrix* inverse(const Matrix* m) {
+    // M * inverse(M) = I
+    // inverse(M) = 1 / det(M) * (adjugate(M))
+    // adjucate(M) = C^T
+    // C = 
+    return NULL;
+}
+
+float determinant(const Matrix* m) {
+    if(isSquare(m) == 0) {
+        printf("Cannot calculate the determinant of a non-square Matrix!");
+        return 0;
+    }
+
+    // base cases
+    if(m->rows == 1 && m->columns == 1) {
+        return m->data[0][0];
+    }
+
+    if(m->rows == 2 && m->columns == 2) {
+        return (m->data[0][0] * m->data[1][1]) - (m->data[0][1] * m->data[1][0]);
+    }
+    // det(M) = Sum(j = 1, n) -1^i+j * Mij * det(matrix excluding ith row and jth col)
+    // j => random column
+    
+    int i = 0;
+    int det = 0;
+    for(int j = 0; j < m->columns; j++) {
+        // create mini matrix
+        Matrix* miniMatrix = createMatrix(m->rows - 1, m->columns - 1);
+        
+        for(int mI = 1; mI < m->rows; mI++) {
+            int mJNew = 0;
+            for(int mJ = 0; mJ < m->columns; mJ++) {
+                if(mJ == j) {
+                    continue;
+                }
+                miniMatrix->data[mI - 1][mJNew] = m->data[mI][mJ];
+                mJNew++;
+            }
+        }
+
+        det += pow(-1, i+j) * m->data[i][j] * determinant(miniMatrix);
+        freeMatrix(miniMatrix);
+    }
+
+    return det;
 }
--- nmath/nmath.h
+++ nmath/nmath.h
@@ -2,11 +2,14 @@
 #define NMATH_H
 
 #include "nmatrix.h"
+#include <math.h>
 
-Matrix* multiply(Matrix* m1, Matrix* m2);
-Matrix* add(Matrix* m1, Matrix* m2);
-Matrix* subtract(Matrix* m1, Matrix* m2);
-Matrix* elementWiseMultiply(Matrix* m1, Matrix* m2);
-Matrix* transpose(Matrix* m);
+Matrix* multiply(const Matrix* m1, const Matrix* m2);
+Matrix* add(const Matrix* m1, const Matrix* m2);
+Matrix* subtract(const Matrix* m1, const Matrix* m2);
+Matrix* elementWiseMultiply(const Matrix* m1, const Matrix* m2);
+Matrix* transpose(const Matrix* m);
+Matrix* inverse(const Matrix* m);
+float determinant(const Matrix* m);
 
 #endif
