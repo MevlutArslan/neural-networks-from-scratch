8ab57e340d7409e01cda010052040dce08fbafcf
MevlutArslan <mevlut12098@gmail.com>
2023-07-31 06:54:50 -0500

    implement thread-pool

--- src/helper/thread_pool.c
+++ src/helper/thread_pool.c
@@ -1,38 +1,141 @@
 #include "thread_pool.h"
-#include <stdlib.h>
-#include "../../libraries/logger/log.h"
 
-TaskQueue* init_task_queue(int capacity) {
-    TaskQueue* t_queue = malloc(sizeof(TaskQueue));
-    t_queue->capacity = capacity;
-    t_queue->number_of_elements = 0;
-    t_queue->tasks = calloc(t_queue->capacity, sizeof(Task));
+void push_task(struct ThreadPool* thread_pool, struct Task* task);
+struct Task* pop_task(struct ThreadPool* thread_pool);
 
-    return t_queue;
+struct ThreadPool* create_thread_pool(int num_threads) {
+    struct ThreadPool* thread_pool = (struct ThreadPool*) malloc(sizeof(struct ThreadPool));
+    thread_pool->is_active = 1;
+    thread_pool->num_threads = num_threads;
+    thread_pool->threads = (pthread_t*) malloc(num_threads * sizeof(pthread_t));
+    thread_pool->num_pending_tasks = 0;
+
+    thread_pool->task_queue = init_task_queue(200);
+
+    for(int i = 0; i < num_threads; i++) {
+        pthread_create(&thread_pool->threads[i], NULL, run_thread, thread_pool);
+    }
+
+    thread_pool->lock = (pthread_mutex_t) PTHREAD_MUTEX_INITIALIZER;
+    thread_pool->pending_tasks_lock = (pthread_mutex_t) PTHREAD_MUTEX_INITIALIZER;
+    thread_pool->capacity_lock = (pthread_mutex_t) PTHREAD_MUTEX_INITIALIZER;
+
+    thread_pool->capacity_signal = (pthread_cond_t) PTHREAD_COND_INITIALIZER;
+    thread_pool->signal = (pthread_cond_t) PTHREAD_COND_INITIALIZER;
+    thread_pool->all_tasks_done = (pthread_cond_t) PTHREAD_COND_INITIALIZER;
+
+    thread_pool->push_task = push_task;
+    thread_pool->pop_task = pop_task;
+
+    return thread_pool;
 }
 
-int add_to_task_queue(TaskQueue* queue, Task* task) {
-    if(queue->number_of_elements >= queue->capacity) {
-        return QUEUE_FULL_ERRROR;
+void* run_thread(void* args) {
+    struct ThreadPool* thread_pool = (struct ThreadPool*) args;
+    struct Task* task = NULL;
+
+    while(thread_pool->is_active == 1) {    
+        pthread_mutex_lock(&thread_pool->lock);
+        
+        while(thread_pool->task_queue->element_count == 0 && thread_pool->is_active == 1){
+            pthread_cond_wait(&thread_pool->signal, &thread_pool->lock);
+        }
+        if(thread_pool->is_active == 0) {
+            pthread_mutex_unlock(&thread_pool->lock);
+            break;
+        }
+        task = thread_pool->pop_task(thread_pool);
+        thread_pool->task_queue->element_count--;
+
+        pthread_mutex_unlock(&thread_pool->lock);
+
+        task->function(task->data);// indicate this task is done.
+        pthread_mutex_lock(&thread_pool->pending_tasks_lock);
+
+
+        thread_pool->num_pending_tasks --;
+        if(thread_pool->num_pending_tasks == 0) {
+            pthread_cond_broadcast(&thread_pool->all_tasks_done);
+        }
+        pthread_mutex_unlock(&thread_pool->pending_tasks_lock);
     }
 
-    queue->tasks[queue->number_of_elements] = task;
-    queue->number_of_elements++;
-
-    return QUEUE_ADD_SUCCESS;
+    return NULL;
 }
 
-Task* pop_from_task_queue(TaskQueue* queue) {
-    if(queue->number_of_elements == 0) {
-        log_error("Task Queue is empty!");
-        return NULL;
+void wait_for_all_tasks(struct ThreadPool* thread_pool) {
+   pthread_mutex_lock(&thread_pool->pending_tasks_lock);
+    while(thread_pool->num_pending_tasks > 0) {
+        pthread_cond_wait(&thread_pool->all_tasks_done, &thread_pool->pending_tasks_lock);
+    }
+    pthread_mutex_unlock(&thread_pool->pending_tasks_lock);
+}
+
+struct TaskQueue* init_task_queue(int capacity) {
+    struct TaskQueue* task_queue = (struct TaskQueue*) calloc(1, sizeof(struct TaskQueue));
+    
+    task_queue->tasks = (struct Task**) malloc(capacity * sizeof(struct Task*));
+    task_queue->capacity = capacity;
+    task_queue->element_count = 0;
+
+    return task_queue;
+}
+
+void push_task(struct ThreadPool* thread_pool, struct Task* task) {
+    pthread_mutex_lock(&thread_pool->lock);
+    while(thread_pool->task_queue->element_count >= thread_pool->task_queue->capacity) {
+        printf("At full capacity, waiting for others tasks to finish...");
+        pthread_cond_wait(&thread_pool->capacity_signal, &thread_pool->capacity_lock);
     }
 
-    Task* popped = queue->tasks[0];
+    thread_pool->task_queue->tasks[thread_pool->task_queue->element_count] = task;
+    thread_pool->task_queue->element_count ++;
+    thread_pool->num_pending_tasks++;
 
-    for(int i = 0; i < queue->number_of_elements - 1; i++) {
-        queue->tasks[i] = queue->tasks[i + 1];
+    pthread_cond_signal(&thread_pool->signal);
+
+    pthread_mutex_unlock(&thread_pool->lock);
+}
+
+
+struct Task* pop_task(struct ThreadPool* thread_pool) {
+    struct Task* task = thread_pool->task_queue->tasks[0];
+
+    for(int i = 0; i < thread_pool->task_queue->element_count - 1; i++) {
+        thread_pool->task_queue->tasks[i] = thread_pool->task_queue->tasks[i + 1];
+    }
+    thread_pool->task_queue->tasks[thread_pool->task_queue->element_count - 1] = NULL;
+    
+    pthread_cond_signal(&thread_pool->capacity_signal);  // signal that a task has been popped from the queue
+
+    return task;
+}
+
+
+void destroy_thread_pool(struct ThreadPool* thread_pool) {
+    thread_pool->is_active = 0;
+
+    pthread_cond_broadcast(&thread_pool->signal);
+
+    for(int i = 0; i < thread_pool->num_threads; i++) {
+        pthread_join(thread_pool->threads[i], NULL);
+    }
+    for(int i = 0; i < thread_pool->task_queue->element_count; i++) {
+        free(thread_pool->task_queue->tasks[i]);
     }
 
-    return popped;
+    free(thread_pool->threads);
+
+    free(thread_pool->task_queue->tasks);
+    free(thread_pool->task_queue);
+
+    pthread_mutex_destroy(&thread_pool->lock);
+    pthread_mutex_destroy(&thread_pool->capacity_lock);
+    pthread_mutex_destroy(&thread_pool->pending_tasks_lock);
+
+    pthread_cond_destroy(&thread_pool->signal);
+    pthread_cond_destroy(&thread_pool->capacity_signal);
+    pthread_cond_destroy(&thread_pool->all_tasks_done);
+    
+    free(thread_pool);
 }
--- src/helper/thread_pool.h
+++ src/helper/thread_pool.h
@@ -2,31 +2,49 @@
 #define THREAD_POOL_H
 
 #include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-#define NUMBER_OF_THREADS 8
-#define MAX_NUMBER_OF_TASKS 256
+struct TaskQueue;
+struct Task;
 
-#define QUEUE_ADD_SUCCESS 1
-#define QUEUE_FULL_ERRROR -1
-#define QUEUE_IS_EMPTY_ERROR -2
+struct ThreadPool {
+    pthread_t* threads;
+    struct TaskQueue* task_queue;
+    
+    int num_threads;
+    volatile int is_active; // flag to start/stop the threads
 
-pthread_t threads[NUMBER_OF_THREADS];
+    int num_pending_tasks;
+    pthread_mutex_t lock; // lock for accessing the queue's element count.
+    pthread_mutex_t capacity_lock; // lock for accesing queue's capacity.
+    pthread_mutex_t pending_tasks_lock; // lock for accessing num_pending_tasks.
 
-typedef struct {
-    // function pointer
-    // task type
-} Task;
+    pthread_cond_t signal; // cond for signaling there are tasks to handle.
+    pthread_cond_t capacity_signal; // cond for signaling the queue has available space for new tasks.
+    pthread_cond_t all_tasks_done; // cond for signaling the thread pool is done with all of its tasks.
 
-// If you need more performance change the data structure to be circular buffer instead of queue.
-typedef struct {
-    Task** tasks;
-    int capacity;
-    int number_of_elements;
-}TaskQueue;
+    void (*push_task)(struct ThreadPool*, struct Task*);
+    struct Task* (*pop_task)(struct ThreadPool*);
+};
 
-TaskQueue* init_task_queue(int capacity);
-int add_to_task_queue(TaskQueue* queue, Task* task);
-Task* pop_from_task_queue(TaskQueue* queue);
+// allocates memory for the thread-pool and initializes properties
+struct ThreadPool* create_thread_pool(int num_threads);
+void destroy_thread_pool(struct ThreadPool* thread_pool);
 
+void* run_thread(void* args);
+void wait_for_all_tasks(struct ThreadPool* thread_pool);
+
+struct TaskQueue {
+    struct Task** tasks; // An array of pointers to tasks
+    int capacity; // maximum number of elements the queue can hold.
+    int element_count; // number of elements in the queue.
+};
+
+struct TaskQueue* init_task_queue(int capacity);
+struct Task {
+    void(*function)(void*); // pointer to the function the task is going to run
+    void* data; // pointer to the data type the function takes
+};
 
 #endif
