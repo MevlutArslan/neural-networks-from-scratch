59890388c1d3a1a7fb2600122d8b98200da58e1e
MevlutArslan <mevlut12098@gmail.com>
2023-08-10 13:33:55 -0500

    can process the entire input matrix at once

--- src/main.c
+++ src/main.c
@@ -17,6 +17,7 @@ void runProgram();
 
 #define TIME_BUFFER_SIZE 64
 
+void mock_forward_pass(Matrix* input_matrix, Matrix** layer_outputs, int num_layers, Matrix** weights, Vector** biases, int begin_index, int end_index);
 FILE* logFile;
 
 void file_log(log_Event *ev) {
@@ -29,6 +30,19 @@ void file_log(log_Event *ev) {
     fflush(logFile);
 }
 
+struct ForwardPassArgs {
+    Matrix* input_matrix;
+    Matrix** layer_outputs;
+    
+    Matrix** weights;
+    Vector** biases;
+
+  
+    int num_layers;
+    int begin_index;
+    int end_index;
+};
+
 int main(int argc, char* argv[])
 {
     logFile = fopen("log.txt", "w");
@@ -40,7 +54,7 @@ int main(int argc, char* argv[])
     // Add the file_log as a callback to the logging library
     log_add_callback(file_log, NULL, LOG_TRACE);
     
-    srand(307); // seeding with 306
+    srand(306); // seeding with 306
 
     int isTesting = 0;
     for (int i = 1; i < argc; i++) {
@@ -60,7 +74,7 @@ int main(int argc, char* argv[])
 }
 
 void runProgram() {
-    // Model* model = create_mnist_model();
+    // Model* model = create_wine_categorization_model();
     // clock_t start = clock();
 
     // model->train_network(model);
@@ -70,27 +84,99 @@ void runProgram() {
     // log_info("time it took to process mnist: %f",  (((double) (end - start)) / CLOCKS_PER_SEC) * 1000);
     // free_model(model);
 
-    struct ThreadPool* thread_pool = create_thread_pool(4);
-    
-    Matrix* matrix = create_matrix(2000, 750);
-    fill_matrix_random(matrix, -45.0f, 45.0f);
+    /* 
+        FORWARD PASS
+        [X]. push through the 'input' matrix via a mock forward pass.
+            [X]. verify the results
+            [X]. dot product
+            [X]. matrix-vector addition
+            [X]. activation function
+        [X]. try to do it without using layer's properties that is not thread safe.
+            [X]. identify properties that are not thread safe
+        ---------------------------------------------------------------------------
+        [X]. modify the mock forward pass to work with "batches" instead of using the entire input matrix
+            [X]. verify the results
+        [-]. run the mini batch version using the thread pool 
+            [-]. verify the results
+        ---------------------------------------------------------------------------
+        [ ]. implement the logic from the mock forward pass into NNetwork
+            [ ]. verify the resuls by comparing them to old version's calculations
 
-    // log_debug("matrix: %s", matrix_to_string(matrix));
-    Vector* vector = create_vector(750);
-    fill_vector_random(vector, -276.0f, 213.0f);
-    
-    Vector* output = create_vector(matrix->rows);
 
-    int begin_index = 0;
-    int end_index = matrix->rows;
+        BACKWARD PASS
 
-    struct MatrixVectorOperation* data = create_matrix_vector_operation(matrix, vector, output, begin_index, end_index);
-    
-    push_dot_product_as_task(thread_pool, data);
+    */
 
-    wait_for_all_tasks(thread_pool);
-    
-    log_info("output vector after tasks are done: %s", vector_to_string(output));
-    log_info("Matrix rows: %d ?= output size: %d", matrix->rows, output->size);
-    destroy_thread_pool(thread_pool);
+
+    // two layers with [4, 2] neurons each
+    int num_layers = 2;
+
+    Matrix* input_matrix = create_matrix(2, 4);
+    fill_matrix_random(input_matrix, -2, 5);
+
+    Matrix* weights_1 = create_matrix(4, input_matrix->columns);
+    fill_matrix_random(weights_1, 0.0f, 1.0f);
+
+    Vector* biases_1 = create_vector(4);
+    fill_vector_random(biases_1, -0.5f, 0.5f);
+
+    Matrix* weights_2 = create_matrix(2, weights_1->rows);
+    fill_matrix_random(weights_2, 0.0f, 1.0f);
+
+    Vector* biases_2 = create_vector(2);
+    fill_vector_random(biases_2, -0.5f, 0.5f);
+
+    Matrix** weights[num_layers];
+    weights[0] = weights_1;
+    weights[1] = weights_2;
+
+    Vector** biases[num_layers];
+    biases[0] = biases_1;
+    biases[1] = biases_2;
+    // input rows x output size
+    Matrix** layer_outputs =  create_matrix_arr(num_layers);
+    Matrix** row_layer_outputs;
+    log_info("Before mock forward pass!");
+
+    log_info("Input Matrix: %s", matrix_to_string(input_matrix));
+    log_info("Weights #1: %s", matrix_to_string(weights_1));
+    log_info("Weights #2 %s", matrix_to_string(weights_2));
+
+    log_info("Biases #1: %s", vector_to_string(biases_1));
+    log_info("Biases #2: %s", vector_to_string(biases_2));
+
+    mock_forward_pass(input_matrix, layer_outputs, num_layers, weights, biases, 0, input_matrix->rows);
 }   
+
+
+void softmax_matrix(Matrix* matrix) {
+    for(int i = 0; i < matrix->rows; i++) {
+        softmax(matrix->data[i]);
+    }
+}
+
+void mock_forward_pass(Matrix* input_matrix, Matrix** layer_outputs, int num_layers, Matrix** weights, Vector** biases, int begin_index, int end_index) { 
+    for(int i = 0; i < num_layers; i++) {
+        Matrix* product;
+        Matrix* transposed_weights = matrix_transpose(weights[i]);
+        
+        if(i == 0) {
+            product = matrix_product(input_matrix, transposed_weights);
+            log_info("product result for first layer: %s", matrix_to_string(product));
+        }else{
+            product = matrix_product(layer_outputs[i - 1], transposed_weights);
+            log_info("product result for second layer: %s", matrix_to_string(product));
+        }
+
+        layer_outputs[i] = matrix_vector_addition(product, biases[i]);
+        log_info("Vector addition results: %s", matrix_to_string(layer_outputs[i]));
+
+        if(i == num_layers - 1) {
+            softmax_matrix(layer_outputs[i]);
+        }else {
+            leakyReluMatrix(layer_outputs[i]);
+        }
+
+        log_info("After activation: %s", matrix_to_string(layer_outputs[i]));
+    }
+}
