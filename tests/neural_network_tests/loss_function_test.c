5291ff9e13887a78febb65a71bc948c69dedcfb2
MevlutArslan <mevlut12098@gmail.com>
2023-09-10 17:56:09 -0500

    add tests for MSE function

---
+++ tests/neural_network_tests/loss_function_test.c
@@ -0,0 +1,53 @@
+#include "loss_function_test.h"
+
+
+void mean_squared_error_perfect_prediction_test() {
+    Matrix* y_values = create_matrix(10, 1);
+    fill_matrix(y_values, 10.0f);
+
+    Matrix* predicted_values = create_matrix(10, 1);
+    fill_matrix(predicted_values, 10.0f);
+
+    double loss = mean_squared_error(predicted_values, y_values);
+
+    assert(loss == 0.0f);
+
+    log_info("Mean Squared Error with perfect prediction, test passed successfully.");
+}
+
+void mean_squared_error_prediction_with_some_error_test() {
+    Matrix* y_values = create_matrix(10, 1);
+    fill_matrix(y_values, 10.0f);
+
+    Matrix* predicted_values = create_matrix(10, 1);
+    fill_matrix(predicted_values, 8.0f);
+
+    double loss = mean_squared_error(predicted_values, y_values);
+
+    // normally it should be 4.0 but because we divide by 2 after each squaring we check against half of the expected.
+    assert(loss == 2.0f); 
+    log_info("Mean Squared Error with some error, test passed successfully.");
+}
+
+void mean_squared_error_derivative_perfect_prediction_test() {
+    double target = 10.0;
+    double predicted = 10.0;
+
+    double derivative = mean_squared_error_derivative(target, predicted);
+
+    // derivative = predicted - target
+    assert(derivative == predicted - target);
+    log_info("Mean Squared Error Derivative with perfect prediction, test passed successfully.");
+}
+
+
+void mean_squared_error_derivative_with_some_error_test() {
+    double target = 10.0;
+    double predicted = 8.0;
+
+    double derivative = mean_squared_error_derivative(target, predicted);
+
+    // derivative = predicted - target i.e. 8 - 10
+    assert(derivative == predicted - target);
+    log_info("Mean Squared Error Derivative with some error, test passed successfully.");
+}
---
+++ tests/neural_network_tests/loss_function_test.h
@@ -0,0 +1,13 @@
+#ifndef LOSS_FUNCTION_H
+#define LOSS_FUNCTION_H
+
+#include <assert.h>
+#include "../../src/neural_network/loss_functions.h"
+
+void mean_squared_error_perfect_prediction_test();
+void mean_squared_error_prediction_with_some_error_test();
+void mean_squared_error_derivative_perfect_prediction_test();
+void mean_squared_error_derivative_with_some_error_test();
+
+
+#endif
