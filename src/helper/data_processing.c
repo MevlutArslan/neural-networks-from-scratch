dd0bfa603df1569f345b8c447d1cc88aebed951d
MevlutArslan <mevlut12098@gmail.com>
2023-09-13 21:41:30 -0500

    fixed bug where empty characters wouldnt be recognized as words

--- src/helper/data_processing.c
+++ src/helper/data_processing.c
@@ -95,14 +95,13 @@ Matrix** load_text_as_embedding(char* file_location, map_t char_int_map, map_t i
             // Handle empty lines or end of file, e.g., by skipping them
             continue;
         }
-
+        // log_info("text:%s", line);
         fill_tokenizer_vocabulary(&line, char_int_map, int_char_map, index);
         embeddings[sentence_index] = line_to_embedding(&line, max_sequence_length, vocab_size, char_int_map);
         sentence_index++;
     }
-
     print_hashmap(char_int_map, CHAR_INT);
-    print_hashmap(int_char_map, INT_CHAR);
+    // print_hashmap(int_char_map, INT_CHAR);
 
     free(index);
 
@@ -118,24 +117,40 @@ Matrix* line_to_embedding(char* text, int max_sequence_length, int vocab_size, m
     int row_index = 0;
 
     while (token != NULL && row_index < sentence_embedding->rows) {
-        word_to_embedding(token, char_int_map, sentence_embedding->data[row_index]);
+        if(row_index == 0 || row_index % 2 == 0) {
+            word_to_embedding(token, char_int_map, sentence_embedding->data[row_index]);
+            token = strtok(NULL, " "); // Move to the next token
+        }else {
+            word_to_embedding(" ", char_int_map, sentence_embedding->data[row_index]);
+        }
+        
         row_index++;
-        token = strtok(NULL, " "); // Move to the next token
     }
+
+    log_info("row index: %d", row_index);
 
     return sentence_embedding;
 }
 
 void word_to_embedding(char* token, map_t char_int_map, Vector* vector) {
+    any_t value;
+
+    if(strcmp(token, " ") == 0) {
+        int rc = hashmap_get(char_int_map, token, &value);
+        assert(rc == MAP_OK);
+
+        vector->elements[0] = (double) (int) value;
+        return;
+    }
+    
     char* stringified_token = malloc(2 * sizeof(char));
     for(int i = 0; i < strlen(token); i++) {
-        if (token[i] == '\0' || token[i] == '\n' || token[i] == '\r') {
+        if (token[i] == '\n' || token[i] == '\r') {
             break;
         }
 
         sprintf(stringified_token, "%c", token[i]);
 
-        any_t value;
 
         int rc = hashmap_get(char_int_map, stringified_token, &value);
         assert(rc == MAP_OK);
